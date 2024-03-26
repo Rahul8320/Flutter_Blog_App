82e51e6256778d6a7935c01522d81762c50e537e
Rahul Dey <rahuldey832000@gmail.com>
2024-03-26 22:43:15 +0530

    Design create new blog screen

--- lib/features/blog/presentation/pages/add_new_blog_page.dart
+++ lib/features/blog/presentation/pages/add_new_blog_page.dart
@@ -1,3 +1,6 @@
+import 'package:blog_app/core/theme/app_pallete.dart';
+import 'package:blog_app/features/blog/presentation/widgets/blog_editor.dart';
+import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 
 class AddNewBlogPage extends StatefulWidget {
@@ -12,17 +15,114 @@ class AddNewBlogPage extends StatefulWidget {
 }
 
 class _AddNewBlogPageState extends State<AddNewBlogPage> {
+  final titleController = TextEditingController();
+  final contentController = TextEditingController();
+  List<String> selectedTopics = [];
+
+  @override
+  void dispose() {
+    super.dispose();
+    titleController.dispose();
+    contentController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.done_rounded),
+        appBar: AppBar(
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.done_rounded),
+            ),
+          ],
+        ),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                DottedBorder(
+                  color: AppPallete.borderColor,
+                  dashPattern: const [10, 4],
+                  radius: const Radius.circular(10),
+                  borderType: BorderType.RRect,
+                  strokeCap: StrokeCap.round,
+                  child: Container(
+                    height: 150,
+                    width: double.infinity,
+                    child: const Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.folder_open,
+                          size: 40,
+                        ),
+                        SizedBox(height: 15),
+                        Text(
+                          "Select your image",
+                          style: TextStyle(
+                            fontSize: 15,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: [
+                      'Technology',
+                      'Business',
+                      'Programming',
+                      'Designing',
+                    ]
+                        .map(
+                          (e) => Padding(
+                            padding: const EdgeInsets.all(5.0),
+                            child: GestureDetector(
+                              onTap: () {
+                                if (selectedTopics.contains(e)) {
+                                  selectedTopics.remove(e);
+                                } else {
+                                  selectedTopics.add(e);
+                                }
+                                setState(() {});
+                              },
+                              child: Chip(
+                                label: Text(e),
+                                color: selectedTopics.contains(e)
+                                    ? const MaterialStatePropertyAll(
+                                        AppPallete.gradient1,
+                                      )
+                                    : null,
+                                side: selectedTopics.contains(e)
+                                    ? null
+                                    : const BorderSide(
+                                        color: AppPallete.borderColor,
+                                      ),
+                              ),
+                            ),
+                          ),
+                        )
+                        .toList(),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                BlogEditor(
+                  hintText: "Blog title",
+                  controller: titleController,
+                ),
+                const SizedBox(height: 10),
+                BlogEditor(
+                  hintText: "Blog content",
+                  controller: contentController,
+                ),
+              ],
+            ),
           ),
-        ],
-      ),
-    );
+        ));
   }
 }
