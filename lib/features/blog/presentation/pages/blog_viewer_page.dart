3662675d8255760added951335d84c30d8de526c
Rahul Dey <rahuldey832000@gmail.com>
2024-04-11 22:30:21 +0530

    Add Blog Viewer Page

---
+++ lib/features/blog/presentation/pages/blog_viewer_page.dart
@@ -0,0 +1,86 @@
+import 'dart:ui';
+
+import 'package:blog_app/core/theme/app_pallete.dart';
+import 'package:blog_app/core/utils/calculate_reading_time.dart';
+import 'package:blog_app/core/utils/formate_date.dart';
+import 'package:blog_app/features/blog/domain/entities/blog.dart';
+import 'package:flutter/material.dart';
+
+class BlogViewerPage extends StatelessWidget {
+  static route(Blog blog) => MaterialPageRoute(
+        builder: (context) => BlogViewerPage(
+          blog: blog,
+        ),
+      );
+
+  final Blog blog;
+  const BlogViewerPage({
+    super.key,
+    required this.blog,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Scrollbar(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  blog.title,
+                  style: const TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(
+                  height: 18,
+                ),
+                Text(
+                  'By ${blog.posterName}',
+                  style: const TextStyle(
+                    fontWeight: FontWeight.w500,
+                    fontSize: 16,
+                  ),
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                Text(
+                  '${formatDateBydMMMYYY(blog.updatedAt)} . ${calculateReadingTime(blog.content)} '
+                  'min',
+                  style: const TextStyle(
+                    color: AppPallete.greyColor,
+                    fontWeight: FontWeight.w500,
+                    fontSize: 16,
+                  ),
+                ),
+                const SizedBox(
+                  height: 18,
+                ),
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Image.network(blog.imageUrl),
+                ),
+                const SizedBox(
+                  height: 18,
+                ),
+                Text(
+                  blog.content,
+                  style: const TextStyle(
+                    fontSize: 16,
+                    height: 1.8,
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
